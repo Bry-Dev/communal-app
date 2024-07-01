6ab066f5e4986f2322acccf421fd64ff0abf7e07
Bry-Dev <bryan.pin3da@gmail.com>
2024-07-02 00:11:55 +0800

    Updates

---
+++ _app/immutable/entry/start.CoLzl8J3.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.B9tLwPlr.js";export{t as start};
--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import ToolList from "../components/ToolList.svelte";
-</script>
-
-<section class="flex flex-col pt-4 gap-4">
-  <ToolList />
-</section>
