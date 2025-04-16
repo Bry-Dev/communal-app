71277089b3abd7c86a3803b6e77f83d3bfb7e633
Bry-Dev <bryan.capati@criterionindustries.com.au>
2025-04-16 13:40:54 +0800

    Updates

---
+++ _app/immutable/chunks/B22r5X45.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1vblir6)==null?void 0:s.base)??"/communal-app";var a;const l=((a=globalThis.__sveltekit_1vblir6)==null?void 0:a.assets)??e;export{l as a,e as b};
