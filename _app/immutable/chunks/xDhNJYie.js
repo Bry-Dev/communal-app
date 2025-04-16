2818edac19759ad8e17f035b14137307222eb8a7
Bry-Dev <bryan.capati@criterionindustries.com.au>
2025-04-16 13:31:27 +0800

    Updates

---
+++ _app/immutable/chunks/xDhNJYie.js
@@ -0,0 +1,1 @@
+import{e}from"./C2k3T2yR.js";e();
--- src/lib/types/AppData.ts
+++
@@ -1,9 +0,0 @@
-export class AppData {
-  code: string = "";
-  title: string = "";
-  description: string = "";
-  image: string = "";
-  isAvailable: boolean = false;
-  link?: string = "";
-  trialLink?: string = "";
-}
